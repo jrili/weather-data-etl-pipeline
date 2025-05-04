c647e75d42ebf8078fed39f25564e8bf99acb10f
jrili <jfmrili@gmail.com>
2025-05-04 15:42:11 +0800

    Finish get_weather script for exploration purposes; - formulat plantuml scripts for ERD and LDM - add scripts/*.json to gitignore

--- scripts/get_weather.sh
+++ scripts/get_weather.sh
@@ -1,21 +1,43 @@
-# Usage: get_weather.sh num_days
-# - num_days: number of days worth data to get, from (today - num_days) up to today
+# Usage: get_weather.sh location date_start date_end
+#
+# e.g get_weather.sh london 2020-01-01 2020-01-02
+#
+# Description:
+#	Get weather daily historical weather data for `location`
+#	from `date_start` to `date_end.
+#	
+# Arguments:
+# - location: string describing a location, e.g. 'manila'
+#	default: manila
+# - date_start: start date in YYYY-mm-dd format
+#	default: current date
+# - date_end: end date in YYYY-mm-dd format
+#	default: current date
+#
 
 
-num_days=$1
-if [[ "$num_days" == "" ]]; then
-	num_days=1
+# Get command-line arguments with defaults
+location=$1
+if [[ "$location" == "" ]]; then
+	location=manila
+fi
+
+date_start=$2
+if [[ "$date_start" == "" ]]; then
+	date_start=$(date +"%Y-%m-%d")
+fi
+date_end=$3
+if [[ "$date_end" == "" ]]; then
+	date_end=$date_start
 fi
 
 echo Get weather data script started
 
-response_filename=response_$(date +"%Y%m%d-%H%M%S").json
+num_days=$(( ($(date -d "$date_end" +%s) - $(date -d "$date_start" +%s))/86400 + 1 ))
+
+response_filename="response_${date_start}_${date_end}.json"
 echo Response will be stored to: $response_filename
 
-date_end=$(date +"%Y-%m-%d")
-date_start=$(date +"%Y-%m-%d" -d "$date_end -$((num_days - 1)) days")
-
-location=manila
 
 url_base="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 
@@ -30,10 +52,29 @@ url="$url/$date_start"
 if [[ $num_days -gt 1 ]]; then
 	url="$url/$date_end"
 fi
-
-url="$url&include=obs,days&elements=temp,tempmax,tempmin,feelslike,feelslikemax,feelslikemin,humidity,precip,uvindex,sunrise,sunset,windspeed"
 echo
-echo "url: $url"
+echo "url (no key, before include): $url"
+
+url_include="include=obs,days&elements=datetime,conditions,temp,tempmax,tempmin,feelslike,feelslikemax,feelslikemin,humidity,precip,snow,uvindex,sunrise,sunset,windspeed"
+echo
+echo "url_include: $url_include"
+
+echo
+response=$(curl -s -w "\\n%{http_code}" "$url?key=$VISUALCROSSING_API_KEY&$url_include")
+
+http_code=$(tail -n1 <<< "$response")
+
+echo
+if [[ "$http_code" == "200" ]]; then
+	content=$(sed '$ d' <<< "$response")
+	echo $content > $response_filename
+	echo Response received successfully, saved to $response_filename
+fi
+
+if [[ "$http_code" != "200" ]]; then
+	echo Received response with not-OK http_code=$http_code. 
+	exit 1
+fi
 
 
-#curl $url?key=$VISUALCROSSING_API_KEY > $response_filename
+
